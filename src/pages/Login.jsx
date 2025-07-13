4917523f7d5655ee86fe835895b67fe83b037edc
Md Belayet Hossain <mdbelayethossain015@gmail.com>
2025-07-13 19:31:53 +0600

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const Login = () => {
     return (
         <div className="lg:max-w-7xl max-w-[350px] mx-auto">
-            
+            <p>this is login page</p>
+            <div className='lg:max-w-2xl mx-auto '>
+                <form>
+                        <input type="email" className="input" placeholder="Email" /><br />
+                        <input type="password" className="input" placeholder="Password" /><br />
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button className="btn btn-neutral mt-4">Login</button>
+                </form>
+            </div>
         </div>
     );
 };
