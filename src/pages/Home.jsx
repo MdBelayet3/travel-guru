a22db6835a926d90f660984840fca07b7ff05f3d
Md belayet <mdbelayethossain016@gmail.com>
2025-07-15 16:48:52 +0600

    updated some file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import Navbar from './shared/Navbar';
+import TouristCarousel from './TouristCarousel';
 
 const Home = () => {
     return (
         <div className="relative h-screen">
             {/* Background image */}
-            <div className="absolute inset-0 bg-[url('/Rectangle1.png')] bg-cover bg-center z-0"></div>
+            <div className="fixed inset-0 bg-[url('/Rectangle1.png')] bg-cover bg-center z-0"></div>
 
             {/* Overlay */}
-            <div className="absolute inset-0 bg-black bg-opacity-80 z-10"></div>
+            <div className="fixed inset-0 bg-black bg-opacity-80 z-10"></div>
 
             {/* Content */}
             <div className="relative z-20">
 
                 <div className="lg:max-w-7xl max-w-[350px] mx-auto">
-                    <Navbar></Navbar>
-                    <p>this is home page</p>
+                    <div className='sticky top-2 bg-[#160929] z-50 '>
+                        <Navbar></Navbar>
+                    </div>
+                    <h1 className='text-center text-4xl font-semibold text-white my-5'>Travel Guru</h1>
+                    <p className='text-white lg:px-40 mb-5'>Travel Guru offers hotel booking, tourist guides, transport, custom tours, and full travel support for a seamless journey experience.</p>
+                    <TouristCarousel></TouristCarousel>
+                    <p className='text-white lg:px-40 pb-32 pt-10'>Travel Guru is a complete travel solution platform offering hotel bookings, tourist guide services, transport arrangements, and custom tour packages. We ensure a smooth, safe, and enjoyable travel experience for all. Whether you're exploring new places or planning a vacation, Travel Guru is here to make your journey effortless and memorable.</p>
                 </div>
             </div>
         </div>
--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -1,41 +1,9 @@
 import React from 'react';
-import imgUrl from '../../assets/images/logo.png'
-import { Link, NavLink } from 'react-router';
 
 const Navbar = () => {
-    const navbar = <>
-        <li><NavLink to="/news">News</NavLink></li>
-        <li><NavLink to="/destination">Destination</NavLink></li>
-        <li><NavLink to="/blog">Blog</NavLink></li>
-        <li><NavLink to="/contact">Contact</NavLink></li>
-    </>
-
     return (
-        <div className="navbar text-white shadow-sm">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-                    </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        {navbar}
-                    </ul>
-                </div>
-                <img className='bg-white p-2 lg:w-32 w-14' src={imgUrl} alt="" />
-            </div>
-            <div className=''>
-                <input type="text" placeholder="Search your destination" className="input bg-[#FFFFFF33] focus:outline-none text-white gap-20 w-24 md:w-auto" />
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    {navbar}
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn bg-orange-400 text-black"><Link to="/login">Login</Link></a>
-            </div>
+        <div>
+            
         </div>
     );
 };
