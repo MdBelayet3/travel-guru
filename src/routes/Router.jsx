a60250905cf629f0403e64ec4ea9ffd2291295be
Md Belayet Hossain <mdbelayethossain015@gmail.com>
2025-07-17 22:08:21 +0600

    Update Router.jsx

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import { createBrowserRouter } from "react-router";
+import Home from "../pages/Home";
+import Root from "../layout/Root";
+import Login from "../pages/Login";
+import Register from "../pages/Register"
+import TouristSpot from "../pages/TouristSpot";
+import Booking from "../pages/Booking";
 
-const Router = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root></Root>,
+        children:[
+            {
+                path: '/',
+                element: <Home></Home>,
+            },
+            {
+                path: "/login",
+                element: <Login></Login>
+            },
+            {
+                path: "/register",
+                element: <Register></Register>
+            },
+            {
+                path: "/tourist/:id",
+                element: <TouristSpot></TouristSpot>,
+                loader: ()=> fetch("/places.json"),
+            },
+            {
+                path: "/tourist/:id/booking",
+                element: <Booking></Booking>
+            }
+        ]
+    }
+])
 
-export default Router;
+export default router;
