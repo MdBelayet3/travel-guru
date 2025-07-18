48636ec2679e5346e1f2a57b768d9b0e39a7d4d4
Md Belayet Hossain <mdbelayethossain015@gmail.com>
2025-07-18 20:44:32 +0600

    Update PrivetRouter.jsx

--- src/routes/PrivetRouter.jsx
+++ src/routes/PrivetRouter.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PrivetRouter = () => {
     return (
         <div>
-            
+            <h1 classname = "text-xl font-bold">This is Privet Router</h1>
         </div>
     );
 };
