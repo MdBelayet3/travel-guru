206918f06fe52a44deafbaaa8fd3c518cedfd28c
Md Belayet Hossain <mdbelayethossain015@gmail.com>
2025-07-22 23:38:46 +0600

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,6 +9,22 @@ const Register = () => {
     // useState hook for eye btn show or not
     const [eyeBtn, setEyeBtn] = useState(false);
 
+    // handle register function
+    const handleRegister = e => {
+        e.preventDefault();
+
+        // to get input data
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget);
+        console.log(form);
+        const name = form.get('name');
+        console.log(name);
+        const photo = form.get('photo');
+        const email = form.get('email');
+        const password = form.get('password');
+        console.log(name, photo, email, password);
+    }
+
     return (
         <div className='bg-[#160929]'>
             <div className='lg:max-w-5xl max-w-[350px] mx-auto'>
@@ -16,7 +32,7 @@ const Register = () => {
             </div>
             <div className="lg:max-w-xl max-w-[350px] mx-auto my-10">
                 <div className='lg:max-w-2xl mx-auto '>
-                    <form className='border-2 rounded-xl bg-white border-gray-300 p-3 lg:px-12 lg:py-8 space-y-5'>
+                    <form onSubmit={handleRegister}  className='border-2 rounded-xl bg-white border-gray-300 p-3 lg:px-12 lg:py-8 space-y-5'>
                         <h1 className='text-2xl font-bold'>Create an account</h1>
                         <input required type="name" className="w-full placeholder-black p-2 border-b-2" placeholder=" First Name" /><br />
                         <input required type="name" className="w-full placeholder-black p-2 border-b-2" placeholder=" Last Name" /><br />
