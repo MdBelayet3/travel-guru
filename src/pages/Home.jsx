207b7e9fc0c5dcc0b3a7b3de0490a35c8d58982d
Md Belayet Hossain <mdbelayethossain015@gmail.com>
2025-07-24 21:26:57 +0600

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
                         <Navbar></Navbar>
                     </div>
                     <h1 className='text-center text-4xl font-semibold text-white my-5'>Travel Guru</h1>
-                    <p className='text-white lg:px-40 mb-5'>Travel Guru offers hotel booking, tourist guides, transport, custom tours, and full travel support for a seamless journey experience.</p>
+                    <p className='text-white lg:px-40 mb-4'>Travel Guru offers hotel booking, tourist guides, transport, custom tours, and full travel support for a seamless journey experience.</p>
                     <TouristCarousel></TouristCarousel>
                     <p className='text-white lg:px-40 pb-32 pt-8'>Travel Guru is a complete travel solution platform offering hotel bookings, tourist guide services, transport arrangements, and custom tour packages. We ensure a smooth, safe, and enjoyable travel experience for all. Whether you're exploring new places or planning a vacation, Travel Guru is here to make your journey effortless and memorable.</p>
                 </div>
